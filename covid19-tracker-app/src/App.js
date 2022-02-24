7119a8c8bfd735d268e777c77f3897295115b350
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-24 19:34:01 +0900

    Application structure created in App.js

--- covid19-tracker-app/src/App.js
+++ covid19-tracker-app/src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="app">
       <h1>Covid19 Tracker Application</h1>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+
     </div>
   );
 }
