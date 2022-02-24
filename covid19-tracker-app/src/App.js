6a481e0f1baef628108b1bbaf46c7ab1620c569c
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-24 20:03:37 +0900

    App Header styled in App.css

--- covid19-tracker-app/src/App.js
+++ covid19-tracker-app/src/App.js
@@ -5,31 +5,34 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>COVID-19 Tracker</h1>
-      <FormControl className="app_dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="worldwide">Fruits</MenuItem>
-          <MenuItem value="worldwide">Apple</MenuItem>
-          <MenuItem value="worldwide">Banana</MenuItem>
-          <MenuItem value="worldwide">Grape</MenuItem>
-        </Select>
-      </FormControl>
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
+      <div className="app_header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="worldwide">Fruits</MenuItem>
+            <MenuItem value="worldwide">Apple</MenuItem>
+            <MenuItem value="worldwide">Banana</MenuItem>
+            <MenuItem value="worldwide">Grape</MenuItem>
+          </Select>
+        </FormControl>
+        {/* Header */}
+        {/* Title + Select input dropdown field */}
 
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
+        {/* InfoBoxes */}
+        {/* InfoBoxes */}
+        {/* InfoBoxes */}
 
-      {/* Table */}
-      {/* Graph */}
+        {/* Table */}
+        {/* Graph */}
 
-      {/* Map */}
+        {/* Map */}
 
+      </div>
     </div>
+
   );
 }
 
