88038179b75e34a5d9bd89e79a71dc9dc5f008b9
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-24 19:45:03 +0900

    Material-UI installed and added

--- covid19-tracker-app/src/App.js
+++ covid19-tracker-app/src/App.js
@@ -1,9 +1,22 @@
+import React from "react"
+import { MenuItem, FormControl, Select, } from "@material-ui/core"
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Covid19 Tracker Application</h1>
+      <h1>COVID-19 Tracker</h1>
+      <FormControl className="app_dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          <MenuItem value="worldwide">Fruits</MenuItem>
+          <MenuItem value="worldwide">Apple</MenuItem>
+          <MenuItem value="worldwide">Banana</MenuItem>
+          <MenuItem value="worldwide">Grape</MenuItem>
+        </Select>
+      </FormControl>
       {/* Header */}
       {/* Title + Select input dropdown field */}
 
