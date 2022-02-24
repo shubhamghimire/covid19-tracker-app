eb412050eda91e295a0a07e3229f96d914e07d4b
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-24 21:13:00 +0900

    got all the countries from API call and map the country on menu item

--- covid19-tracker-app/src/App.js
+++ covid19-tracker-app/src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { MenuItem, FormControl, Select, } from "@material-ui/core"
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState([
-    'Nepal', 'Japan', 'Korea'
-  ]);
+  const [countries, setCountries] = useState([]);
 
+  // https://disease.sh/v3/covid-19/countries
+
+  // USEEFFECT = Runs a piece of code
+  // based a given condition
+
+  useEffect(() => {
+    // The code inside here will run once
+    // When the component loads and not again
+    // async -> send a request to a server, wait for it, do something with it
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          // [item1, item2, item3]
+          // item 1 ... -> returning an object in a shape
+          // item 2 ... -> returning an object in a shape
+          const countries = data.map((country) => (
+            {
+              name: country.country,  // Japan, United Kingdom, Nepal
+              value: country.countryInfo.iso2 //JPN, UK, NP
+            }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, [countries])
 
   return (
     <div className="app">
@@ -20,7 +44,7 @@ function App() {
             {/* Loope through all the countries and list all the countries */}
             {
               countries.map(country =>
-                <MenuItem value={country}>{country}</MenuItem>)
+                <MenuItem value={country.value}>{country.name}</MenuItem>)
             }
           </Select>
         </FormControl>
