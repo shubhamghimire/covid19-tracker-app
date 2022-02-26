02ecfbb12c6cf8c08892d79fb1b060b8c344ddce
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-26 19:53:50 +0900

    InfoBox.js structure created

--- covid19-tracker-app/src/components/InfoBox.js
+++ covid19-tracker-app/src/components/InfoBox.js
@@ -1,11 +1,18 @@
 // Type 'rfce' to create react functional component
 import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
 function InfoBox({ title, cases, total }) {
     return (
-        <div>
+        <Card>
+            <CardContent>
+                {/* Title i.e. Coronavirus Cases */}
 
-        </div>
+                {/* +90 k Number of cases */}
+
+                {/* 2.5M Total */}
+            </CardContent>
+        </Card>
     )
 }
 
