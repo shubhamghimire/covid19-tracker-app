b6bc8acfea1cf0b86ba24c03ab187eb380e6be0e
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-26 20:00:26 +0900

    InfoBox component content added

--- covid19-tracker-app/src/components/InfoBox.js
+++ covid19-tracker-app/src/components/InfoBox.js
@@ -4,13 +4,18 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 
 function InfoBox({ title, cases, total }) {
     return (
-        <Card>
+        <Card className="infoBox">
             <CardContent>
                 {/* Title i.e. Coronavirus Cases */}
+                <Typography className="infoBox_title" color="textSecondary">
+                    {title}
+                </Typography>
 
                 {/* +90 k Number of cases */}
+                <h2 className="infoBox_cases">{cases}</h2>
 
                 {/* 2.5M Total */}
+                <Typography className="infoBox_total" color="textSecondary">{total} Total</Typography>
             </CardContent>
         </Card>
     )
