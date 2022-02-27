33c222d181cb0573ef8ae9d225cf1a50ed0e456a
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-27 13:58:36 +0900

    getting and organizing historical data to parse into line chart

--- covid19-tracker-app/src/components/LineGraph.js
+++ covid19-tracker-app/src/components/LineGraph.js
@@ -4,6 +4,28 @@ import { Line } from "react-chartjs-2";
 function LineGraph() {
     const [data, setData] = useState({});
 
+    // Organizing historical data to parse into Line chart
+    const buildChartData = (data, casesType = "cases") => {
+        const chartData = [];
+        let lastDataPoint;
+
+        for (let date in data.cases) {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date,
+                    // Subtracting previous days cases from today's cases
+                    // So that we can get new cases
+                    y: data['cases'][date] - lastDataPoint
+                }
+
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data[casesType][date];
+        }
+        console.log("Chart Data>>>", chartData)
+        return chartData;
+    }
+
     // Getting all historical data for last 50 days
     // https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
@@ -11,15 +33,24 @@ function LineGraph() {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+                console.log("Graph Data>>>", data);
+                const chartData = buildChartData(data);
+                setData(chartData);
             })
     }, [])
+
 
     return (
         <div>
             {/* Line data options */}
             <h1>This is Graph!</h1>
-
+            <Line data={{
+                datasets: [{
+                    data: data,
+                    backgroundColor: "rgba(204, 16, 52, 0.5)",
+                    borderColor: "#CC1034",
+                }]
+            }} />
         </div>
     )
 }
