9f21b95ecc330eb817e0724e2a28a18995f1fc1e
shubhamghimire <shubhamghimire1996@gmail.com>
2022-03-05 21:49:23 +0900

    InfoBoxes styling added

--- covid19-tracker-app/src/components/InfoBox.js
+++ covid19-tracker-app/src/components/InfoBox.js
@@ -1,5 +1,6 @@
 // Type 'rfce' to create react functional component
-import React from 'react'
+import React from 'react';
+import './InfoBox.css';
 import { Card, CardContent, Typography } from '@material-ui/core'
 
 function InfoBox({ title, cases, total }) {
